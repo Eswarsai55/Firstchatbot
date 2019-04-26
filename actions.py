aaef9cb7842ce8a20d68767b9e95d5235549121f
Eswar sai <49666994+Eswarsai55@users.noreply.github.com>
2019-04-26 15:12:21 +0530

    Add files via upload

---
+++ actions.py
@@ -0,0 +1,45 @@
+from rasa_core.actions import Action
+from rasa_core.events import Restarted
+from rasa_core.actions.forms import (
+    BooleanFormField,
+    EntityFormField,
+    FormAction,
+    FreeTextFormField
+)
+from rasa_core_sdk.events import SlotSet
+import httplib2
+import requests
+
+
+class ActionappIssue(FormAction):
+
+	RANDOMIZE=False
+	
+	@staticmethod
+	def required_fields():
+		return[
+		EntityFormField("application", "application"),
+		EntityFormField("description", "description")
+		]
+		
+	def name(self):
+		return "action_application"
+		
+	def submit(self,dispatcher,tracker,domain):
+		s=str(tracker.get_slot("application"))
+		url="https://www."+s+".com"
+		return[]
+		
+class ActionurlIssue(FormAction):
+
+	RANDOMIZE=False
+	
+	@staticmethod
+	def required_fields():
+		return[
+		EntityFormField("url", "url"),
+		EntityFormField("description", "description")
+		]
+		
+	def name(self):
+		return "action_url"
